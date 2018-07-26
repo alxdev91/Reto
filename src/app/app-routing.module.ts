702a202cf6cf0a9bea7346f4ff89c315c13d37b2
Julio Isabel Martinez Leal <julio.leal@neoris.cxnetworks.net>
2018-07-26 17:26:11 -0500

    Creacion de Rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { UsuarioComponent } from './usuario/usuario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'usuarios', component: UsuariosComponent,
+    children: [
+      { path: 'usuario', component: UsuarioComponent }
+    ]
+  },
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
